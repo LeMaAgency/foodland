a9cffbbba5bfcb0ab1b5cb119330218b5f117e88
LeMa <kopojiq@gmail.com>
2017-11-24 16:16:10 +0300

    remove extra data (not need) & add comments

--- local/templates/.default/components/bitrix/news.list/cheese_elements/result_modifier.php
+++ local/templates/.default/components/bitrix/news.list/cheese_elements/result_modifier.php
@@ -4,20 +4,24 @@ if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 
 use \WM\Common\Helper;
 
+//collect all section ids for select from iblock
 $sections = array();
 foreach($arResult['ITEMS'] as $k => $arItem)
 {
     if(Helper::propFilled('CATALOG_SECTIONS', $arItem))
         $sections = array_merge($sections, Helper::propValue('CATALOG_SECTIONS', $arItem));
 }
+//remove duples
 $sections = array_unique($sections);
+//reorganize array data
 $sections = array_fill_keys($sections, array());
 
+//search sections by its id
 $iblockSections = \WM\IBlock\Section::getList(3, array(
     'filter' => array('ACTIVE' => 'Y', 'ID' => array_keys($sections)),
     'arSelect' => array('ID', 'NAME', 'SECTION_PAGE_URL'),
 ));
-
+//collect section data
 foreach($iblockSections as $section)
 {
     $sections[$section['ID']] = array(
@@ -26,7 +30,7 @@ foreach($iblockSections as $section)
         'URL' => $section['SECTION_PAGE_URL'],
     );
 }
-
+//add section data to items
 foreach($arResult['ITEMS'] as $k => $arItem)
 {
     $itemSections = array();
--- local/templates/.default/components/bitrix/news.list/our_clients/result_modifier.php
+++ local/templates/.default/components/bitrix/news.list/our_clients/result_modifier.php
@@ -1,13 +1,3 @@
 <?php
 if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
     die();
-
-use \WM\Common\Helper;
-
-foreach($arResult['ITEMS'] as $k => $arItem)
-{
-    if(Helper::propFilled('LOGO_IMG', $arItem))
-        $arResult['ITEMS'][$k]['PROPERTIES']['LOGO_IMG']['VALUE_SRC'] = \CFile::GetPath(Helper::propValue('LOGO_IMG', $arItem));
-    if(Helper::propFilled('LOGO_TITLE_IMG', $arItem))
-        $arResult['ITEMS'][$k]['PROPERTIES']['LOGO_TITLE_IMG']['VALUE_SRC'] = \CFile::GetPath(Helper::propValue('LOGO_TITLE_IMG', $arItem));
-}
