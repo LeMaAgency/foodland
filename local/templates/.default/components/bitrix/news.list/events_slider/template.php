7593514c4f5c29a3f5626af56726faa9026dd71f
Если чё я Леха <alexgreenvi@yandex.ru>
2017-12-04 15:41:47 +0300

    Правки слайлера

--- local/templates/.default/components/bitrix/news.list/events_slider/template.php
+++ local/templates/.default/components/bitrix/news.list/events_slider/template.php
@@ -26,7 +26,27 @@ $data = new \WM\Template\TemplateHelper($this);
 ?>
 <div class="product-use-carousel product-use-carousel_m-0-50">
     <div class="container">
-        <div class="row">
+        <div class="row product-use-carousel_4__carousel">
+            <? foreach($data->items() as $item):
+            if(!$item->previewPicture())
+                continue;
+            ?>
+                <div class="product-use-carousel__wrap">
+                    <div class="product-use-carousel__carousel">
+                            <div class="product-use-carousel__carousel__item" <?=$item->editId();?>>
+                                <img src="<?=$item->previewPicture();?>" alt="<?=$item->getName();?>">
+                                <div class="product-use-carousel__carousel__item__name"><?=$item->getName();?></div>
+                                <div class="product-use-carousel__carousel__item__block-link">
+                                    <a href="<?=$item->detailUrl();?>" title="<?=$item->getName();?>">
+                                        <span><?=Loc::getMessage('LEMA_DETAIL_SLIDER_EVENT_MORE_LINK');?></span>
+                                    </a>
+                                </div>
+                            </div>
+
+                    </div>
+                </div>
+            <? endforeach; ?>
+            <? /*
             <div class="product-use-carousel__wrap">
                 <div class="product-use-carousel__carousel">
                     <? foreach($data->items() as $item):
@@ -81,24 +101,7 @@ $data = new \WM\Template\TemplateHelper($this);
                     <? endforeach; ?>
                 </div>
             </div>
-            <div class="product-use-carousel__wrap">
-                <div class="product-use-carousel__carousel">
-                    <? foreach($data->items() as $item):
-                        if(!$item->previewPicture())
-                            continue;
-                        ?>
-                        <div class="product-use-carousel__carousel__item" <?=$item->editId();?>>
-                            <img src="<?=$item->previewPicture();?>" alt="<?=$item->getName();?>">
-                            <div class="product-use-carousel__carousel__item__name"><?=$item->getName();?></div>
-                            <div class="product-use-carousel__carousel__item__block-link">
-                                <a href="<?=$item->detailUrl();?>" title="<?=$item->getName();?>">
-                                    <span><?=Loc::getMessage('LEMA_DETAIL_SLIDER_EVENT_MORE_LINK');?></span>
-                                </a>
-                            </div>
-                        </div>
-                    <? endforeach; ?>
-                </div>
-            </div>
+            */ ?>
         </div>
     </div>
 </div>
