cdb1d58cbb2d3f9942eb051aae73007bf18df9a4
DoNotEvenThink <alekseivasin1993@gmail.com>
2017-12-04 17:17:22 +0300

    add news-list "popular-exclusive"

---
+++ local/templates/.default/components/bitrix/news.list/popular_exclusive/template.php
@@ -0,0 +1,62 @@
+<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+
+/** @var CBitrixComponent $component */
+
+use \Bitrix\Main\Localization\Loc;
+
+Loc::loadMessages(__FILE__);
+
+$this->setFrameMode(true);
+
+if(empty($arResult['ITEMS']))
+    return;
+
+$data = new \WM\Template\TemplateHelper($this);
+
+?>
+<div class="popular ">
+    <div class="back-img back-img__maslo"></div>
+    <div class="container">
+        <div class="title-big"><?=Loc::getMessage('LEMA_MOST_POPULAR_MAIN_TITLE');?></div>
+        <div class="popular__carousel">
+            <? foreach($data->items() as $item): ?>
+                <a href="<?=$item->detailUrl();?>" title="<?=$item->getName();?>">
+                    <div class="popular__item" <?=$item->editId();?>>
+                        <div class="popular__item__image">
+                            <img src="<?=$item->previewPicture();?>" alt="<?=$item->getName();?>">
+                        </div>
+                        <div class="popular__item__name"><b><?=$item->getName();?></b></div>
+                        <div class="popular__item__text">
+                            <span><?=$item->prop('BRAND', 'LINKED_NAME');?></span><br>
+                            <? if($item->propFilled('FATNESS')): ?>
+                                <span>
+                                    <?=$item->propName('FATNESS');?>:
+                                    <b><?=$item->propValue('FATNESS');?></b>
+                                </span><br>
+                            <? endif; ?>
+                            <? if($item->propFilled('ARTICLE')): ?>
+                                <span>
+                                    <?=$item->propName('ARTICLE');?>:
+                                    <b><?=$item->propValue('ARTICLE');?></b>
+                                </span><br>
+                            <? endif; ?>
+                        </div>
+                        <div class="popular__item__description">
+                            <p><?=$item->previewText();?></p>
+                        </div>
+                    </div>
+                </a>
+            <? endforeach; ?>
+        </div>
+    </div>
+</div>
