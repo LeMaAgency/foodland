e40588825c280c7f5148c30d2cc3491657532868
LeMa <kopojiq@gmail.com>
2017-12-08 18:32:34 +0300

    recipes add ingredients

--- local/templates/.default/components/bitrix/news/recipes/bitrix/news.detail/.default/template.php
+++ local/templates/.default/components/bitrix/news/recipes/bitrix/news.detail/.default/template.php
@@ -22,16 +22,114 @@ $data = new \WM\Template\TemplateHelper($this);
 
 $item = $data->item();
 ?>
-<article class="text-brand">
-    <p>
-        <?=$item->detailText();?>
-    </p>
-    <? if($item->propFilled('URL')): ?>
-        <div class="text-brand__link">
-            <?=Loc::getMessage('LEMA_BRAND_DETAIL_SITE_LINK');?>
-            <a href="<?=$item->propValue('URL');?>" title="<?=$item->getName();?>"><?=$item->prop('URL', 'DESCRIPTION');?></a>
+<section class="one-recipe">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-24">
+                <h1 class="one-recipe__h1"><?=$arResult['NAME'];?></h1>
+                <div class="one-recipe__ingredients">
+                    <div class="block-product-img__description">
+                        <h2 class="block-product-img__description__h2">Ингредиенты:</h2>
+                        <div class="block-product-img__description__line block-product-img__description__line_full">
+                            <div class="block-product-img__description__line__name">Брынза</div>
+                            <div class="block-product-img__description__line__dots"></div>
+                            <div class="block-product-img__description__line__val"><span>100</span>г</div>
+                        </div>
+                        <div class="block-product-img__description__line block-product-img__description__line_full">
+                            <div class="block-product-img__description__line__name">Помидоры</div>
+                            <div class="block-product-img__description__line__dots"></div>
+                            <div class="block-product-img__description__line__val"><span>1</span>шт.</div>
+                        </div>
+                        <div class="block-product-img__description__line block-product-img__description__line_full">
+                            <div class="block-product-img__description__line__name">Сливочное растительное масло</div>
+                            <div class="block-product-img__description__line__dots"></div>
+                            <div class="block-product-img__description__line__val"><span>20</span>г</div>
+                        </div>
+                        <div class="block-product-img__description__line block-product-img__description__line_full">
+                            <div class="block-product-img__description__line__name">Яйца</div>
+                            <div class="block-product-img__description__line__dots"></div>
+                            <div class="block-product-img__description__line__val"><span>2</span>шт.</div>
+                        </div>
+                        <div class="block-product-img__description__line block-product-img__description__line_full">
+                            <div class="block-product-img__description__line__name">Болгарский перец</div>
+                            <div class="block-product-img__description__line__dots"></div>
+                            <div class="block-product-img__description__line__val"><span>1-2</span>шт.</div>
+                        </div>
+                        <div class="block-product-img__description__line block-product-img__description__line_full">
+                            <div class="block-product-img__description__line__name">Базилик</div>
+                        </div>
+                        <div class="block-product-img__description__line block-product-img__description__line_full">
+                            <div class="block-product-img__description__line__name">Сельдерей сушеный</div>
+                        </div>
+                        <div class="block-product-img__description__line block-product-img__description__line_full">
+                            <div class="block-product-img__description__line__name">Петрушка (зелень)</div>
+                        </div>
+                        <div class="block-product-img__description__line block-product-img__description__line_full">
+                            <div class="block-product-img__description__line__name">Укроп (зелень)</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-24">
+                <div class="one-recipe__img">
+                    <img src="/assets/images/eggs.png" alt="">
+                </div>
+                <div class="one-recipe__info-icon">
+                    <div class="one-recipe__info-icon__item one-recipe__info-icon__item_time">
+                        <span><?=$item->propValue('TIME');?></span>мин.
+                    </div>
+                    <div class="one-recipe__info-icon__item one-recipe__info-icon__item_category">
+                        <?=$arResult['SECTION']['PATH'][0]['NAME'];?>
+                    </div>
+                    <a href="" title="">
+                        <div class="one-recipe__info-icon__item one-recipe__info-icon__item_print">
+                            Распечатать рецепт
+                        </div>
+                    </a>
+                </div>
+            </div>
         </div>
-    <? endif; ?>
-</article>
-<?php
-$this->SetViewTarget('brand_logo_detail');?><img src="/assets/images/slider/1-logo.png" alt="<?=$item->getName();?>"><?$this->EndViewTarget();
+    </div>
+</section>
+<div class="container">
+    <div class="row">
+        <div class="col-17 col-md-24">
+            <div class="text-one-recipe">
+                <h3 class="text-one-recipe__h3">Приготовление</h3>
+                <p>На дно кокотниц выложить по кусочку сливочного масла, либо капнуть по паре капелек растительного. Помидор нарезать маленькими кубиками. Из половинки перца вырезать ножом или выемкой для печенья 4 сердечка, остатки мелко порубить и смешать с помидорами.</p>
+                <p>Кусок брынзы разделить пополам, отщипнуть от каждой по небольшому кусочку (мы будем посыпать этими крошками блюдо сверху).</p>
+                <p>Брынзу выложить на дно кокотниц. Накрыть сыр смесью помидоров и перца, в каждую из кокотниц влить по яйцу. Сверху присыпать сухими травами и крошками брынзы. На них выложить по 2 сердечка из перца и украсить свежей зеленью. При желании, слои можно начать с помидорных кубиков, потом брынза, снова помидоры, яйцо, травы, посыпка брынзой и перец.</p>
+                <p>При желании, слои можно начать с помидорных кубиков, потом брынза, снова помидоры, яйцо, травы, посыпка брынзой и перец. Так - тоже очень вкусно.</p>
+                <p>Поместить кокотницы в духовку, довести до температуры в 200 С, запекать в течение нескольких минут, до момента, когда брынза подплавится и появится характерный запах готового блюда.</p>
+                <p>Подавать горячим.</p>
+                <p>Блюдо получается очень эффектным, ярким, с неожиданным вкусом и начинкой: получается густым, творожно-сырным, солоноватым, с легкой кислинкой и радостными вкраплениями сочных овощей и зелени. Яйцо смягчает остроту брынзы и объединяет блюдо в очень вкусную запеканку.</p>
+                <p>Приятного Вам аппетита!</p>
+            </div>
+        </div>
+        <div class="col-7 col-md-24">
+            <div class="photo-product-recipe">
+                <div class="photo-product-recipe__h3">Фото используемых продуктов:</div>
+                <div class="product-use-carousel__carousel product-use-carousel__carousel_one-recipe">
+                    <div class="product-use-carousel__carousel__item product-use-carousel__carousel__item_m-h">
+                        <img src="/assets/images/product/product-page/main-img--product.png" alt="">
+                    </div>
+                    <div class="product-use-carousel__carousel__item product-use-carousel__carousel__item_m-h">
+                        <img src="/assets/images/product/product-page/main-img--product.png" alt="">
+                    </div>
+                </div>
+                <div class="random__button random__button_one-recipe">
+                    <a href="" title="" class="button-orange button-orange_one-recipe"><span>В каталог продуктов</span></a>
+                </div>
+                <div class="photo-product-recipe__h3">Вам может быть интересно:</div>
+                <div class="product-use-carousel__carousel product-use-carousel__carousel_one-recipe">
+                    <div class="product-use-carousel__carousel__item product-use-carousel__carousel__item_m-h">
+                        <img src="/assets/images/product/product-page/main-img--product.png" alt="">
+                    </div>
+                    <div class="product-use-carousel__carousel__item product-use-carousel__carousel__item_m-h">
+                        <img src="/assets/images/product/product-page/main-img--product.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- local/templates/.default/components/bitrix/news/recipes/search.php
+++ local/templates/.default/components/bitrix/news/recipes/search.php
@@ -13,78 +13,43 @@
 
 $this->setFrameMode(true);
 ?>
-<? $APPLICATION->IncludeComponent('bitrix:news.list', 'brands_top_slider', array(
-    'DISPLAY_DATE' => 'Y',
-    'DISPLAY_NAME' => 'Y',
-    'DISPLAY_PICTURE' => 'Y',
-    'DISPLAY_PREVIEW_TEXT' => 'Y',
-    'AJAX_MODE' => 'N',
-    'IBLOCK_TYPE' => 'content',
-    'IBLOCK_ID' => '15',
-    'NEWS_COUNT' => '20',
-    'SORT_BY1' => 'ACTIVE_FROM',
-    'SORT_ORDER1' => 'DESC',
-    'SORT_BY2' => 'SORT',
-    'SORT_ORDER2' => 'ASC',
-    'FILTER_NAME' => '',
-    'FIELD_CODE' => array(),
-    'PROPERTY_CODE' => array(),
-    'CHECK_DATES' => 'Y',
-    'DETAIL_URL' => '',
-    'PREVIEW_TRUNCATE_LEN' => '',
-    'ACTIVE_DATE_FORMAT' => 'd.m.Y',
-    'SET_TITLE' => 'N',
-    'SET_BROWSER_TITLE' => 'N',
-    'SET_META_KEYWORDS' => 'N',
-    'SET_META_DESCRIPTION' => 'N',
-    'SET_LAST_MODIFIED' => 'N',
-    'INCLUDE_IBLOCK_INTO_CHAIN' => 'N',
-    'ADD_SECTIONS_CHAIN' => 'N',
-    'HIDE_LINK_WHEN_NO_DETAIL' => 'Y',
-    'PARENT_SECTION' => '',
-    'PARENT_SECTION_CODE' => '',
-    'INCLUDE_SUBSECTIONS' => 'Y',
-    'CACHE_TYPE' => 'A',
-    'CACHE_TIME' => '36000000',
-    'CACHE_FILTER' => 'Y',
-    'CACHE_GROUPS' => 'N',
-    'DISPLAY_TOP_PAGER' => 'N',
-    'DISPLAY_BOTTOM_PAGER' => 'N',
-    'PAGER_TITLE' => 'Элементы',
-    'PAGER_SHOW_ALWAYS' => 'N',
-    'PAGER_TEMPLATE' => '',
-    'PAGER_DESC_NUMBERING' => 'N',
-    'PAGER_DESC_NUMBERING_CACHE_TIME' => '36000',
-    'PAGER_SHOW_ALL' => 'N',
-    'PAGER_BASE_LINK_ENABLE' => 'N',
-    'SET_STATUS_404' => 'N',
-    'SHOW_404' => 'N',
-    'MESSAGE_404' => '',
-    'PAGER_BASE_LINK' => '',
-    'PAGER_PARAMS_NAME' => 'arrPager',
-    'AJAX_OPTION_JUMP' => 'N',
-    'AJAX_OPTION_STYLE' => 'Y',
-    'AJAX_OPTION_HISTORY' => 'N',
-    'AJAX_OPTION_ADDITIONAL' => '',
-)); ?>
+
 <div class="background-page">
     <? if($arParams["USE_SEARCH"] == "Y"): ?>
         <div class="container">
             <div class="element">
-                <h1 class="element__title__h1"><? $APPLICATION->ShowTitle(false); ?></h1>
+                <h1 class="h1 h1_mt-35"><? $APPLICATION->ShowTitle(false); ?></h1>
             </div>
             <? $APPLICATION->IncludeComponent(
                 "bitrix:search.form",
                 "flat",
                 Array(
                     "PAGE" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["search"],
-                    'REQUEST_QUERY' => (empty($_GET['q']) ? '' : $_GET['q']),
                 ),
                 $component,
                 array('HIDE_ICONS' => 'Y')
             ); ?>
         </div>
     <? endif ?>
+
+    <? $APPLICATION->IncludeComponent('bitrix:catalog.section.list', 'recipes_section_list', array(
+        'VIEW_MODE' => 'TEXT',
+        'SHOW_PARENT_NAME' => 'Y',
+        'IBLOCK_TYPE' => $arParams['IBLOCK_TYPE'],
+        'IBLOCK_ID' => $arParams['IBLOCK_ID'],
+        'SECTION_ID' => '',
+        'SECTION_CODE' => '',
+        'SECTION_URL' => '',
+        'COUNT_ELEMENTS' => 'Y',
+        'TOP_DEPTH' => '2',
+        'SECTION_FIELDS' => array('CODE', 'PICTURE'),
+        'SECTION_USER_FIELDS' => '',
+        'ADD_SECTIONS_CHAIN' => 'Y',
+        'CACHE_TYPE' => 'A',
+        'CACHE_TIME' => '36000000',
+        'CACHE_NOTES' => '',
+        'CACHE_GROUPS' => 'Y',
+    )); ?>
 
     <div class="container">
         <div class="row">
@@ -104,6 +69,7 @@ $this->setFrameMode(true);
                             "CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
                             "PAGER_PARAMS_NAME" => $arParams["PAGER_PARAMS_NAME"],
                             'FORM_ACTION' => $arParams['SEF_FOLDER'],
+                            'ADDITIONAL_CLASS' => 'filter_mb-30',
                         ),
                         $component
                     );
@@ -111,91 +77,91 @@ $this->setFrameMode(true);
                 </div>
             <? endif ?>
             <div class="container-main">
-                <?if(empty($_GET['q'])):?>
-                <? $APPLICATION->IncludeComponent(
-                    "bitrix:news.list",
-                    "brand_list",
-                    Array(
-                        "IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
-                        "IBLOCK_ID" => $arParams["IBLOCK_ID"],
-                        "NEWS_COUNT" => $arParams["NEWS_COUNT"],
-                        "SORT_BY1" => $arParams["SORT_BY1"],
-                        "SORT_ORDER1" => $arParams["SORT_ORDER1"],
-                        "SORT_BY2" => $arParams["SORT_BY2"],
-                        "SORT_ORDER2" => $arParams["SORT_ORDER2"],
-                        "FIELD_CODE" => $arParams["LIST_FIELD_CODE"],
-                        "PROPERTY_CODE" => $arParams["LIST_PROPERTY_CODE"],
-                        "DISPLAY_PANEL" => $arParams["DISPLAY_PANEL"],
-                        "SET_TITLE" => $arParams["SET_TITLE"],
-                        "SET_LAST_MODIFIED" => $arParams["SET_LAST_MODIFIED"],
-                        "MESSAGE_404" => $arParams["MESSAGE_404"],
-                        "SET_STATUS_404" => $arParams["SET_STATUS_404"],
-                        "SHOW_404" => $arParams["SHOW_404"],
-                        "FILE_404" => $arParams["FILE_404"],
-                        "INCLUDE_IBLOCK_INTO_CHAIN" => $arParams["INCLUDE_IBLOCK_INTO_CHAIN"],
-                        "ADD_SECTIONS_CHAIN" => $arParams["ADD_SECTIONS_CHAIN"],
-                        "CACHE_TYPE" => $arParams["CACHE_TYPE"],
-                        "CACHE_TIME" => $arParams["CACHE_TIME"],
-                        "CACHE_FILTER" => $arParams["CACHE_FILTER"],
-                        "CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
-                        "DISPLAY_TOP_PAGER" => $arParams["DISPLAY_TOP_PAGER"],
-                        "DISPLAY_BOTTOM_PAGER" => $arParams["DISPLAY_BOTTOM_PAGER"],
-                        "PAGER_TITLE" => $arParams["PAGER_TITLE"],
-                        "PAGER_TEMPLATE" => $arParams["PAGER_TEMPLATE"],
-                        "PAGER_SHOW_ALWAYS" => $arParams["PAGER_SHOW_ALWAYS"],
-                        "PAGER_DESC_NUMBERING" => $arParams["PAGER_DESC_NUMBERING"],
-                        "PAGER_DESC_NUMBERING_CACHE_TIME" => $arParams["PAGER_DESC_NUMBERING_CACHE_TIME"],
-                        "PAGER_SHOW_ALL" => $arParams["PAGER_SHOW_ALL"],
-                        "PAGER_BASE_LINK_ENABLE" => $arParams["PAGER_BASE_LINK_ENABLE"],
-                        "PAGER_BASE_LINK" => $arParams["PAGER_BASE_LINK"],
-                        "PAGER_PARAMS_NAME" => $arParams["PAGER_PARAMS_NAME"],
-                        "DISPLAY_DATE" => $arParams["DISPLAY_DATE"],
-                        "DISPLAY_NAME" => "Y",
-                        "DISPLAY_PICTURE" => $arParams["DISPLAY_PICTURE"],
-                        "DISPLAY_PREVIEW_TEXT" => $arParams["DISPLAY_PREVIEW_TEXT"],
-                        "PREVIEW_TRUNCATE_LEN" => $arParams["PREVIEW_TRUNCATE_LEN"],
-                        "ACTIVE_DATE_FORMAT" => $arParams["LIST_ACTIVE_DATE_FORMAT"],
-                        "USE_PERMISSIONS" => $arParams["USE_PERMISSIONS"],
-                        "GROUP_PERMISSIONS" => $arParams["GROUP_PERMISSIONS"],
-                        "FILTER_NAME" => $arParams["FILTER_NAME"],
-                        "HIDE_LINK_WHEN_NO_DETAIL" => $arParams["HIDE_LINK_WHEN_NO_DETAIL"],
-                        "CHECK_DATES" => $arParams["CHECK_DATES"],
-                        "STRICT_SECTION_CHECK" => $arParams["STRICT_SECTION_CHECK"],
+                <? if(empty($_GET['q'])): ?>
+                    <? $APPLICATION->IncludeComponent(
+                        "bitrix:news.list",
+                        "recipes_list_main",
+                        Array(
+                            "IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
+                            "IBLOCK_ID" => $arParams["IBLOCK_ID"],
+                            "NEWS_COUNT" => $arParams["NEWS_COUNT"],
+                            "SORT_BY1" => $arParams["SORT_BY1"],
+                            "SORT_ORDER1" => $arParams["SORT_ORDER1"],
+                            "SORT_BY2" => $arParams["SORT_BY2"],
+                            "SORT_ORDER2" => $arParams["SORT_ORDER2"],
+                            "FIELD_CODE" => $arParams["LIST_FIELD_CODE"],
+                            "PROPERTY_CODE" => $arParams["LIST_PROPERTY_CODE"],
+                            "DISPLAY_PANEL" => $arParams["DISPLAY_PANEL"],
+                            "SET_TITLE" => $arParams["SET_TITLE"],
+                            "SET_LAST_MODIFIED" => $arParams["SET_LAST_MODIFIED"],
+                            "MESSAGE_404" => $arParams["MESSAGE_404"],
+                            "SET_STATUS_404" => $arParams["SET_STATUS_404"],
+                            "SHOW_404" => $arParams["SHOW_404"],
+                            "FILE_404" => $arParams["FILE_404"],
+                            "INCLUDE_IBLOCK_INTO_CHAIN" => $arParams["INCLUDE_IBLOCK_INTO_CHAIN"],
+                            "ADD_SECTIONS_CHAIN" => $arParams["ADD_SECTIONS_CHAIN"],
+                            "CACHE_TYPE" => $arParams["CACHE_TYPE"],
+                            "CACHE_TIME" => $arParams["CACHE_TIME"],
+                            "CACHE_FILTER" => $arParams["CACHE_FILTER"],
+                            "CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
+                            "DISPLAY_TOP_PAGER" => $arParams["DISPLAY_TOP_PAGER"],
+                            "DISPLAY_BOTTOM_PAGER" => $arParams["DISPLAY_BOTTOM_PAGER"],
+                            "PAGER_TITLE" => $arParams["PAGER_TITLE"],
+                            "PAGER_TEMPLATE" => $arParams["PAGER_TEMPLATE"],
+                            "PAGER_SHOW_ALWAYS" => $arParams["PAGER_SHOW_ALWAYS"],
+                            "PAGER_DESC_NUMBERING" => $arParams["PAGER_DESC_NUMBERING"],
+                            "PAGER_DESC_NUMBERING_CACHE_TIME" => $arParams["PAGER_DESC_NUMBERING_CACHE_TIME"],
+                            "PAGER_SHOW_ALL" => $arParams["PAGER_SHOW_ALL"],
+                            "PAGER_BASE_LINK_ENABLE" => $arParams["PAGER_BASE_LINK_ENABLE"],
+                            "PAGER_BASE_LINK" => $arParams["PAGER_BASE_LINK"],
+                            "PAGER_PARAMS_NAME" => $arParams["PAGER_PARAMS_NAME"],
+                            "DISPLAY_DATE" => $arParams["DISPLAY_DATE"],
+                            "DISPLAY_NAME" => "Y",
+                            "DISPLAY_PICTURE" => $arParams["DISPLAY_PICTURE"],
+                            "DISPLAY_PREVIEW_TEXT" => $arParams["DISPLAY_PREVIEW_TEXT"],
+                            "PREVIEW_TRUNCATE_LEN" => $arParams["PREVIEW_TRUNCATE_LEN"],
+                            "ACTIVE_DATE_FORMAT" => $arParams["LIST_ACTIVE_DATE_FORMAT"],
+                            "USE_PERMISSIONS" => $arParams["USE_PERMISSIONS"],
+                            "GROUP_PERMISSIONS" => $arParams["GROUP_PERMISSIONS"],
+                            "FILTER_NAME" => $arParams["FILTER_NAME"],
+                            "HIDE_LINK_WHEN_NO_DETAIL" => $arParams["HIDE_LINK_WHEN_NO_DETAIL"],
+                            "CHECK_DATES" => $arParams["CHECK_DATES"],
+                            "STRICT_SECTION_CHECK" => $arParams["STRICT_SECTION_CHECK"],
 
-                        "PARENT_SECTION" => $arResult["VARIABLES"]["SECTION_ID"],
-                        "PARENT_SECTION_CODE" => $arResult["VARIABLES"]["SECTION_CODE"],
-                        "DETAIL_URL" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["detail"],
-                        "SECTION_URL" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["section"],
-                        "IBLOCK_URL" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["news"],
-                    ),
-                    $component
-                ); ?>
-                <?else:?>
-                    <?$APPLICATION->IncludeComponent(
+                            "PARENT_SECTION" => $arResult["VARIABLES"]["SECTION_ID"],
+                            "PARENT_SECTION_CODE" => $arResult["VARIABLES"]["SECTION_CODE"],
+                            "DETAIL_URL" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["detail"],
+                            "SECTION_URL" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["section"],
+                            "IBLOCK_URL" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["news"],
+                        ),
+                        $component
+                    ); ?>
+                <? else: ?>
+                    <? $APPLICATION->IncludeComponent(
                         "bitrix:search.page",
                         "",
                         Array(
-                            "CHECK_DATES" => $arParams["CHECK_DATES"]!=="N"? "Y": "N",
-                            "arrWHERE" => Array("iblock_".$arParams["IBLOCK_TYPE"]),
-                            "arrFILTER" => Array("iblock_".$arParams["IBLOCK_TYPE"]),
+                            "CHECK_DATES" => $arParams["CHECK_DATES"] !== "N" ? "Y" : "N",
+                            "arrWHERE" => Array("iblock_" . $arParams["IBLOCK_TYPE"]),
+                            "arrFILTER" => Array("iblock_" . $arParams["IBLOCK_TYPE"]),
                             "SHOW_WHERE" => "N",
                             //"PAGE_RESULT_COUNT" => "",
                             "CACHE_TYPE" => $arParams["CACHE_TYPE"],
                             "CACHE_TIME" => $arParams["CACHE_TIME"],
                             "SET_TITLE" => $arParams["SET_TITLE"],
-                            "arrFILTER_iblock_".$arParams["IBLOCK_TYPE"] => Array($arParams["IBLOCK_ID"]),
+                            "arrFILTER_iblock_" . $arParams["IBLOCK_TYPE"] => Array($arParams["IBLOCK_ID"]),
                             'IBLOCK_TYPE' => $arParams['IBLOCK_TYPE'],
                             'IBLOCK_ID' => $arParams['IBLOCK_ID'],
                         ),
                         $component
-                    );?>
-                <?endif;?>
+                    ); ?>
+                <? endif; ?>
             </div>
         </div>
     </div>
-    <div class="background-page__tomat_brand"></div>
-    <div class="background-page__block-left-top_brand"></div>
-    <div class="background-page__block-left-middle_brand"></div>
-    <div class="background-page__block-right_brand"></div>
-    <div class="background-page__cheese-right_brand"></div>
+    <div class="background-page__cheese-left_recipe"></div>
+    <div class="background-page__block-left-top_recipe"></div>
+    <div class="background-page__block-left-middle_recipe"></div>
+    <div class="background-page__cow-right"></div>
+    <div class="background-page__block-right-top_recipe"></div>
 </div>
