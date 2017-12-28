ba015f49f6b1d845c247f5cbd207ca735bb30f9c
DoNotEvenThink <alekseivasin1993@gmail.com>
2017-12-28 17:39:08 +0300

    edit bottom slider template "our_clients"

--- local/templates/.default/components/bitrix/news.list/our_clients/template.php
+++ local/templates/.default/components/bitrix/news.list/our_clients/template.php
@@ -24,14 +24,16 @@ $data = new \WM\Template\TemplateHelper($this);
     <div class="back-img back-img__petro back-img__petro_left"></div>
     <div class="back-img back-img__petro back-img__petro_right"></div>
     <div class="container">
-        <h3 class="clients__title"><?=$data->getName();?></h3>
+        <h3 class="clients__title">
+            <?=$data->getName();?>
+        </h3>
         <div class="clients__carousel">
             <? foreach($data->items() as $item): ?>
-                <a href="<?=$item->detailUrl();?>" title="<?=$item->getName();?>">
+                <span title="<?=$item->getName();?>">
                     <div class="clients__carousel__item" <?=$item->editId();?>>
                         <img src="<?=$item->previewPicture();?>" alt="<?=$item->getName();?>">
                     </div>
-                </a>
+                </span>
             <? endforeach; ?>
         </div>
     </div>
