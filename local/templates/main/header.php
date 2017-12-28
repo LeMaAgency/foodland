909aecb7d3ce86fe9730215a48a576f29a20b8d0
Если чё я Леха <alexgreenvi@yandex.ru>
2017-12-28 11:26:20 +0300

    favicon

--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -15,6 +15,8 @@ Loc::loadMessages(__FILE__);
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport'/>
 
+    <!-- Icons -->
+    <link rel="icon" type="image/png" href="/favicon.png"/>
     <?php
     //init js & css
     $assetManager = new \WM\Common\AssetManager();
